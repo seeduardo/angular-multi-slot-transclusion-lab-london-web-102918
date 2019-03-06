a76eeba04af36416863b4f5759b9a4bde53d551e
seeduardo <romenflatiron@gmail.com>
2019-03-06 12:26:33 +0000

    Lab completed

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,10 +1,16 @@
 function UserProfile() {
 	return {
-		transclude: true,
+		transclude: {
+			name: 'h4',
+			position: 'h6',
+			description: 'p'
+		},
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
+				'Name: <div ng-transclude="name"></div>',
+				'Position: <div ng-transclude="position"></div>',
+				'Description: <div ng-transclude="description"></div>',
 			'</div>'
 		].join('')
 	}
